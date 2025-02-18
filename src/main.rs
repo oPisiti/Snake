5b86dbb9de4e982c4c18d36fa2e0e4ec9d10f090
leah <laurohrfranca@gmail.com>
2025-02-17 21:23:30 -0300

    Rendering to screen

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-// Global constants
+// Global constantS
 const BOARD_HEIGHT: usize = 5;
-const BOARD_WIDTH: usize = 5;
+const BOARD_WIDTH: usize = 7;
+const BOARD_CHAR: &str = "#";
+const SNAKE_CHAR: &str = "*";
+const SNAKE_HEAD_CHAR: &str = "@";
 
 fn main() {
-    // Constants
-    const BOARD_CHAR: &str = "#";
-    const SNAKE_CHAR: &str = "*";
-    const SNAKE_HEAD_CHAR: &str = "@";
-
     let board = [BOARD_CHAR; BOARD_WIDTH * BOARD_HEIGHT];
+    let mut snake: Vec<[usize; 2]> = vec![[3, 2], [0, 2], [0, 0]];
 
-    let curr_board = board;
+    // Copy the board for editing
+    let mut curr_board = board;
+
+    // Make changes to the board
+    put_snake_to_board(&snake, &mut curr_board);
+
+    // Final rendering
     print_board(&curr_board);
 }
 
@@ -27,6 +32,49 @@ fn print_board(board: &[&str]) {
     }
 }
 
-// Print board
-// Update snake's vertices
-// Update board
+fn put_snake_to_board(snake: &[[usize; 2]], board: &mut [&str]) {
+    let mut last_vertex = &snake[0];
+    let mut row_boundaries;
+    let mut col_boundaries;
+
+    // Body
+    for vertex in snake.iter().skip(1).take(snake.len()) {
+        let mut delta: [usize; 2] = [0, 0];
+
+        // Set deltas
+        if vertex[0] == last_vertex[0] {
+            delta[1] = 1;
+        } else {
+            delta[0] = 1;
+        }
+
+        // Set starting and ending vertices
+        row_boundaries = sorted(last_vertex[0], vertex[0]);
+        col_boundaries = sorted(last_vertex[1], vertex[1]);
+
+        // Put to board
+        let mut curr_r = row_boundaries.0;
+        let mut curr_c = col_boundaries.0;
+
+        while curr_r < row_boundaries.1 || curr_c < col_boundaries.1 {
+            let index = curr_r * BOARD_WIDTH + curr_c;
+            board[index] = SNAKE_CHAR;
+
+            curr_r += delta[0];
+            curr_c += delta[1];
+        }
+
+        last_vertex = vertex;
+    }
+
+    // Head
+    board[snake[0][0] * BOARD_WIDTH + snake[0][1]] = SNAKE_HEAD_CHAR;
+}
+
+#[inline]
+fn sorted<T: std::cmp::PartialOrd>(a: T, b: T) -> (T, T) {
+    if a <= b {
+        return (a, b);
+    }
+    (b, a)
+}
